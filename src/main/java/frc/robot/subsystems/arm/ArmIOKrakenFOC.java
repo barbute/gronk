1bb4e449f4eeb654532952b63804e55bed3d736c
barbute <jackhdemeterio@gmail.com>
2024-09-09 14:26:41 -0500

    Instantiate arm motors

--- src/main/java/frc/robot/subsystems/arm/ArmIOKrakenFOC.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOKrakenFOC.java
@@ -4,4 +4,23 @@
 
 package frc.robot.subsystems.arm;
 
-public class ArmIOKrakenFOC implements ArmIO {}
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class ArmIOKrakenFOC implements ArmIO {
+  private final TalonFX LEAD_MOTOR;
+  private final TalonFX FOLLOW_MOTOR;
+  private final CANcoder ABSOLUTE_ENCODER;
+
+  private final TalonFXConfiguration MOTOR_CONFIG = new TalonFXConfiguration();
+
+  public ArmIOKrakenFOC() {
+    LEAD_MOTOR = new TalonFX(ArmConstants.LEAD_MOTOR_ID);
+    FOLLOW_MOTOR = new TalonFX(ArmConstants.FOLLOW_MOTOR_ID);
+    FOLLOW_MOTOR.setControl(new Follower(ArmConstants.LEAD_MOTOR_ID, true));
+
+    ABSOLUTE_ENCODER = new CANcoder(ArmConstants.ABSOLUTE_ENCODER_ID);
+  }
+}
