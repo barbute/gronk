bf4e674947ee3dd6bed83a79c8ad1043bead0562
barbute <jackhdemeterio@gmail.com>
2024-10-01 14:31:02 -0500

    Confgure arm motors and sensors

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.Constants;
 
@@ -11,12 +12,26 @@ import frc.robot.Constants;
 public class ArmConstants {
   public record ArmGains(double P, double I, double D, double S, double V, double A, double G) {}
 
+  public record KrakenConfiguration(
+      boolean LEADER_INVERT,
+      boolean LEADER_ENABLE_STATOR_CURRENT_LIMIT,
+      boolean LEADER_ENABLE_SUPPLY_CURRENT_LIMIT,
+      double LEADER_STATOR_CURRENT_LIMIT_AMP,
+      double LEADER_SUPPLY_CURRENT_LIMIT_AMP,
+      double LEADER_PEAK_FORWARD_TORQUE_CURRENT_LIMIT_AMP,
+      double LEADER_PEAK_REVERSE_TORQUE_CURRENT_LIMIT_AMP,
+      NeutralModeValue LEADER_NEUTRAL_MODE) {}
+
   public static final int LEAD_MOTOR_ID = 41;
   public static final int FOLLOW_MOTOR_ID = 42;
   public static final int ABSOLUTE_ENCODER_PORT = 2; // TODO Get this
 
+  /*
+   * rotations = (meters/(2*pi*wheelRadiusMeters))*gearRatio
+   * meters = (rotations/gearRatio)*(2*pi*wheelRadiusMeters)
+   */
   public static final double GEAR_RATIO = 96.9 / 1.0;
-  public static final Rotation2d ABSOLUTE_ENCODER_OFFSET = new Rotation2d();
+  public static final Rotation2d ABSOLUTE_ENCODER_OFFSET = new Rotation2d(); // TODO Get this
 
   public static final ArmGains ARM_GAINS =
       switch (Constants.CURRENT_MODE) {
@@ -25,5 +40,8 @@ public class ArmConstants {
         default -> new ArmGains(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
       };
 
-  public static final Rotation2d POSITION_TOLERANCE = Rotation2d.fromDegrees(3.0);
+  public static final KrakenConfiguration ARM_CONFIGURATION =
+      new KrakenConfiguration(true, true, true, 80.0, 30.0, 80.0, -80.0, NeutralModeValue.Brake);
+
+  public static final Rotation2d POSITION_TOLERANCE = Rotation2d.fromDegrees(1.0);
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIOKrakenFOC.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOKrakenFOC.java
@@ -4,22 +4,99 @@
 
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.robot.subsystems.arm.ArmConstants.KrakenConfiguration;
+import java.util.List;
 
 public class ArmIOKrakenFOC implements ArmIO {
+  // Follow motor follows lead motor exactly, so we only need to write code for the leader
   private final TalonFX LEAD_MOTOR;
   private final TalonFX FOLLOW_MOTOR;
   private final DutyCycleEncoder ABSOLUTE_ENCODER;
 
-  private final TalonFXConfiguration MOTOR_CONFIG = new TalonFXConfiguration();
+  private final StatusSignal<Double> INTERNAL_POSITION_ROTATION;
+  private final StatusSignal<Double> VELOCITY_ROTATION_PER_SEC;
+  private final List<StatusSignal<Double>> APPLIED_VOLTAGE;
+  private final List<StatusSignal<Double>> SUPPLY_CURRENT_AMP;
+  private final List<StatusSignal<Double>> TEMPERATURE_CELSIUS;
+  private final List<StatusSignal<Double>> TORQUE_CURRENT_AMP;
 
-  public ArmIOKrakenFOC() {
+  private final VoltageOut VOLTAGE_CONTROL =
+      new VoltageOut(0.0).withEnableFOC(true).withUpdateFreqHz(0.0);
+  private final TorqueCurrentFOC TORQUE_CURRENT_CONTROL =
+      new TorqueCurrentFOC(0.0).withUpdateFreqHz(0.0);
+  private final PositionTorqueCurrentFOC POSSITION_TORQUE_CURRENT_CONTROL =
+      new PositionTorqueCurrentFOC(0.0).withUpdateFreqHz(0.0);
+
+  private final TalonFXConfiguration LEAD_MOTOR_CONFIG = new TalonFXConfiguration();
+
+  public ArmIOKrakenFOC(KrakenConfiguration krakenConfiguration) {
     LEAD_MOTOR = new TalonFX(ArmConstants.LEAD_MOTOR_ID);
     FOLLOW_MOTOR = new TalonFX(ArmConstants.FOLLOW_MOTOR_ID);
     FOLLOW_MOTOR.setControl(new Follower(ArmConstants.LEAD_MOTOR_ID, true));
     ABSOLUTE_ENCODER = new DutyCycleEncoder(ArmConstants.ABSOLUTE_ENCODER_PORT);
+
+    LEAD_MOTOR_CONFIG.Slot0.kP = ArmConstants.ARM_GAINS.P();
+    LEAD_MOTOR_CONFIG.Slot0.kI = ArmConstants.ARM_GAINS.I();
+    LEAD_MOTOR_CONFIG.Slot0.kD = ArmConstants.ARM_GAINS.D();
+
+    LEAD_MOTOR_CONFIG.CurrentLimits.StatorCurrentLimit =
+        krakenConfiguration.LEADER_STATOR_CURRENT_LIMIT_AMP();
+    LEAD_MOTOR_CONFIG.CurrentLimits.StatorCurrentLimitEnable =
+        krakenConfiguration.LEADER_ENABLE_STATOR_CURRENT_LIMIT();
+    LEAD_MOTOR_CONFIG.CurrentLimits.SupplyCurrentLimit =
+        krakenConfiguration.LEADER_SUPPLY_CURRENT_LIMIT_AMP();
+    LEAD_MOTOR_CONFIG.CurrentLimits.SupplyCurrentLimitEnable =
+        krakenConfiguration.LEADER_ENABLE_SUPPLY_CURRENT_LIMIT();
+    LEAD_MOTOR_CONFIG.TorqueCurrent.PeakForwardTorqueCurrent =
+        krakenConfiguration.LEADER_PEAK_FORWARD_TORQUE_CURRENT_LIMIT_AMP();
+    LEAD_MOTOR_CONFIG.TorqueCurrent.PeakReverseTorqueCurrent =
+        krakenConfiguration.LEADER_PEAK_REVERSE_TORQUE_CURRENT_LIMIT_AMP();
+    LEAD_MOTOR_CONFIG.MotorOutput.Inverted =
+        krakenConfiguration.LEADER_INVERT()
+            ? InvertedValue.Clockwise_Positive
+            : InvertedValue.CounterClockwise_Positive;
+    LEAD_MOTOR_CONFIG.MotorOutput.NeutralMode = krakenConfiguration.LEADER_NEUTRAL_MODE();
+
+    // Use internal sensor after reset; Because the internal sensor makes use of the Kraken's output
+    // shaft to count rotations (and the motor is geared) we must account for this when using
+    // internal PID control
+    LEAD_MOTOR_CONFIG.Feedback.RotorToSensorRatio = ArmConstants.GEAR_RATIO;
+    LEAD_MOTOR_CONFIG.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+
+    LEAD_MOTOR.getConfigurator().apply(LEAD_MOTOR_CONFIG, 1.0);
+
+    INTERNAL_POSITION_ROTATION = LEAD_MOTOR.getPosition();
+    VELOCITY_ROTATION_PER_SEC = LEAD_MOTOR.getVelocity();
+    APPLIED_VOLTAGE = List.of(LEAD_MOTOR.getMotorVoltage(), FOLLOW_MOTOR.getMotorVoltage());
+    SUPPLY_CURRENT_AMP = List.of(LEAD_MOTOR.getSupplyCurrent(), FOLLOW_MOTOR.getSupplyCurrent());
+    TORQUE_CURRENT_AMP = List.of(LEAD_MOTOR.getTorqueCurrent(), FOLLOW_MOTOR.getTorqueCurrent());
+    TEMPERATURE_CELSIUS = List.of(LEAD_MOTOR.getDeviceTemp(), FOLLOW_MOTOR.getDeviceTemp());
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        100,
+        INTERNAL_POSITION_ROTATION,
+        VELOCITY_ROTATION_PER_SEC,
+        APPLIED_VOLTAGE.get(0),
+        APPLIED_VOLTAGE.get(1),
+        SUPPLY_CURRENT_AMP.get(0),
+        SUPPLY_CURRENT_AMP.get(1),
+        TORQUE_CURRENT_AMP.get(0),
+        TORQUE_CURRENT_AMP.get(1),
+        TEMPERATURE_CELSIUS.get(0),
+        TEMPERATURE_CELSIUS.get(1));
+
+    LEAD_MOTOR.optimizeBusUtilization(0.0, 1.0);
+    FOLLOW_MOTOR.optimizeBusUtilization(0.0, 1.0);
   }
 }
