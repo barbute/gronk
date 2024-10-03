386928bc067938ec9ef403471247e23dba6e47c2
barbute <jackhdemeterio@gmail.com>
2024-10-03 15:58:37 -0500

    Move sim constants to ArmConstants.java

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -22,6 +22,14 @@ public class ArmConstants {
       double LEADER_PEAK_REVERSE_TORQUE_CURRENT_LIMIT_AMP,
       NeutralModeValue LEADER_NEUTRAL_MODE) {}
 
+  public record SimulationConfiguration(
+      boolean SIMULATE_GRAVITY, // Sim grav since it's just a regular arm
+      double J_KG_METER_SQUARED,
+      double ARM_LENGTH_METERS,
+      Rotation2d MIN_ARM_ANGLE,
+      Rotation2d MAX_ARM_ANGLE,
+      Rotation2d START_ARM_ANGLE) {}
+
   public static final int LEAD_MOTOR_ID = 41;
   public static final int FOLLOW_MOTOR_ID = 42;
   public static final int ABSOLUTE_ENCODER_PORT = 2; // TODO Get this
@@ -32,6 +40,7 @@ public class ArmConstants {
    */
   public static final double GEAR_RATIO = 96.9 / 1.0;
   public static final Rotation2d ABSOLUTE_ENCODER_OFFSET = new Rotation2d(); // TODO Get this
+  public static final Rotation2d SIMULATION_ARM_POSITION_OFFSET = new Rotation2d();
 
   public static final ArmGains ARM_GAINS =
       switch (Constants.CURRENT_MODE) {
@@ -42,6 +51,15 @@ public class ArmConstants {
 
   public static final KrakenConfiguration ARM_CONFIGURATION =
       new KrakenConfiguration(true, true, true, 80.0, 30.0, 80.0, -80.0, NeutralModeValue.Brake);
+
+  public static final SimulationConfiguration SIMULATION_ARM_CONFIGURATION =
+      new SimulationConfiguration(
+          true,
+          0.0,
+          0.0,
+          Rotation2d.fromDegrees(0.0),
+          Rotation2d.fromDegrees(0.0),
+          Rotation2d.fromDegrees(0.0));
 
   public static final Rotation2d POSITION_TOLERANCE = Rotation2d.fromDegrees(1.0);
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
@@ -12,7 +13,26 @@ public class ArmIOSim implements ArmIO {
 
   private final SingleJointedArmSim ARM =
       new SingleJointedArmSim(
-          DCMotor.getKrakenX60(2), ArmConstants.GEAR_RATIO, 0.0, 0.0, 0.0, 0.0, false, 0.0);
+          DCMotor.getKrakenX60(2),
+          ArmConstants.GEAR_RATIO,
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.J_KG_METER_SQUARED(),
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.ARM_LENGTH_METERS(),
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.MIN_ARM_ANGLE().getRadians(),
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.MAX_ARM_ANGLE().getRadians(),
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.SIMULATE_GRAVITY(),
+          ArmConstants.SIMULATION_ARM_CONFIGURATION.START_ARM_ANGLE().getRadians());
 
-  public ArmIOSim() {}
+  private final PIDController FEEDBACK = new PIDController(ArmConstants.ARM_GAINS.P(), ArmConstants.ARM_GAINS.I(), ArmConstants.ARM_GAINS.D());
+
+  private double positionOffsetRad = ArmConstants.SIMULATION_ARM_POSITION_OFFSET.getRadians();
+  private double appliedVoltage = 0.0;
+
+  public ArmIOSim() {
+    ARM.setState(0.0, 0.0);
+    setPosition(0.0);
+  }
+
+  private void setPosition(double positionRad) {
+    positionOffsetRad = positionRad - ARM.getAngleRads();
+  }
 }
